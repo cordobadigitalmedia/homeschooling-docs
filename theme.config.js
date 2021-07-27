b59876abc4b823c71549ad7fddf6a9da14607a9a
Cordoba Digital Media <66155382+cordobadigitalmedia@users.noreply.github.com>
2021-07-27 05:46:15 +0300

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/cordobadigitalmedia/homeschooling-docs',
+  titleSuffix: ' – Homeschool',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Homeschooling</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Resources for Parents
       </span>
     </>
   ),
@@ -61,5 +61,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Cordoba Digital Media.</>,
 }
